79579a935fe20e7be978bbca81afa18d13504f96
Maxx Lehmann <mglehman@mtu.edu>
2018-06-17 23:55:48 +0200

    renders head

--- src/App.js
+++ src/App.js
@@ -1,21 +1,112 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { ...song }
+
+  onSplitChange = e => this.setState({
+    head: {
+      ...this.state.head,
+      [e.target.name]: e.target.value,
+    }
+  })
+
+  onBarChange = e => {
+    let newBars = [...this.state.head.bars]
+    newBars[e.target.name] = e.target.value
+    this.setState({
+      head: {
+        ...this.state.head,
+        bars: newBars,
+      }
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <React.Fragment>
+        <header>
+          <h2>Bar Intentions</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <main>
+          <h3>{this.state.name}</h3>
+          <bar-commit>
+            <HeadHeader data={this.state.head} change={this.onSplitChange} />
+            <hr />
+            <Head data={this.state.head} change={this.onBarChange} />
+          </bar-commit>
+        </main>
+      </React.Fragment>
     );
   }
 }
 
+const Head = props => props.data.bars.map((value, i) =>
+  <bar-line key={i}>
+    <input
+      name={i}
+      value={value}
+      onChange={props.change}
+    />
+    <button>+</button>
+    {(i + 1) % props.data.split === 0 ? <space-out /> : null}
+  </bar-line>
+)
+
+const HeadHeader = props => (
+  <head-header>
+    <span>Version: HEAD</span>
+    <span className="commit-right">
+      Split:
+      <input
+        type="number"
+        value={props.data.split}
+        className="split"
+        name="split"
+        onChange={props.change}
+      />
+      <button>commit</button>
+    </span>
+  </head-header>
+)
+
 export default App;
+
+var song = {
+  name: "Notorious",
+  author: "maxx",
+  head: {
+    split: 2,
+    bars: [
+      "Spit your game, talk your stuff",
+      "Grab your gun and your clique and squeeze",
+      "Pass that weed, I gotta fight",
+      "All them hoes, I got to like one",
+      "Our situation is a tight one",
+      "now what? fight or run?",
+    ],
+  },
+  commits: [
+    {
+      commited: 1529052866575,
+      split: 1,
+      bars: [
+        "Spit your weed, talk your game",
+        "I got to like one hoe",
+        "Jump or kick?",
+      ]
+    },
+    {
+      split: 2,
+      commited: 1529253863575,
+      bars: [
+        "Spit your game, talk your stuff",
+        "Grab your gun and your clique and squeeze",
+        "Pass that weed, I gotta fight",
+        "All them hoes, I got to like one",
+        "Our situation is a tight one",
+        "now what? fight or run?",
+      ]
+    },
+  ],
+}
